e98e38ab2a307c3e62af2097ac8cab2383ec1236
Andrew Masiye <amasiye313@gmail.com>
2026-02-05 03:57:59 +0200

    feat(exception): add SceneManagementException for handling scene management errors

---
+++ src/Exceptions/Scenes/SceneManagementException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sendama\Engine\Exceptions\Scenes;
+
+use Sendama\Engine\Exceptions\SendamaException;
+
+/**
+ * Represents an exception thrown during a critical scene management action.
+ */
+class SceneManagementException extends SendamaException
+{
+}
