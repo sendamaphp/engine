340331c4e3f5f1cc918fb3c97fdcfd6db9fed254
Andrew Masiye <amasiye313@gmail.com>
2026-02-27 20:19:41 +0200

    feat(GameObject): add serialization and unserialization methods for game objects

--- src/Core/Scenes/SceneManager.php
+++ src/Core/Scenes/SceneManager.php
@@ -21,6 +21,7 @@ use Sendama\Engine\Events\SceneEvent;
 use Sendama\Engine\Exceptions\IncorrectComponentTypeException;
 use Sendama\Engine\Exceptions\Scenes\SceneManagementException;
 use Sendama\Engine\Exceptions\Scenes\SceneNotFoundException;
+use Sendama\Engine\Physics\Collider;
 use Sendama\Engine\Physics\Interfaces\ColliderInterface;
 use Sendama\Engine\Physics\Physics;
 use Sendama\Engine\UI\Label\Label;
@@ -350,6 +351,10 @@ final class SceneManager implements SingletonInterface, CanStart, CanResume, Can
         $sceneName = $sceneMetadata->name ?? basename($path);
 
         $scene = new class($sceneName, $sceneMetadata) extends AbstractScene {
+            /**
+             * @return void
+             * @throws SceneManagementException
+             */
             public function awake(): void
             {
                 $sceneMetadata = $this->sceneMetadata;
@@ -435,7 +440,10 @@ final class SceneManager implements SingletonInterface, CanStart, CanResume, Can
                                                     continue;
                                                 }
 
-                                                $component->$key = $value;
+                                                $component->$key = match(true) {
+                                                    $componentClass === Collider::class && $key === 'material' => null,
+                                                    default => $value
+                                                };
                                             }
                                         }
                                     }
